576777244cbdefbbad86f7a67dd7d787862ece7d
Matthew Harris <rtpharry@hotmail.com>
2023-03-28 23:16:41 +0100

    initial commit

---
+++ runthings-secrets.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Secrets
+Plugin URI: https://runthings.dev
+Description: Share secrets securely
+Version: 1.0.0
+Author: Matthew Harris, runthings.dev
+Author URI: https://runthings.dev/
+*/
