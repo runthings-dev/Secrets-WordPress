5bda39c55d62468802953f61431c69ec88f20df9
Matthew Harris <rtpharry@hotmail.com>
2023-03-28 23:25:42 +0100

    chore(legal): add license

--- runthings-secrets.php
+++ runthings-secrets.php
@@ -6,4 +6,28 @@ Description: Share secrets securely
 Version: 1.0.0
 Author: Matthew Harris, runthings.dev
 Author URI: https://runthings.dev/
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Copyright 2023 Matthew Harris
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 3, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+
+public function secret_sharing_plugin_init() {
+    // Your initialization code goes here.
+}
+
+add_action('init', 'secret_sharing_plugin_init');
