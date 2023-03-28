4120b4bdda8345369309abc0af98e97eeb67512d
Matthew Harris <rtpharry@hotmail.com>
2023-03-28 23:40:43 +0100

    feat(activate): create table

--- runthings-secrets.php
+++ runthings-secrets.php
@@ -24,10 +24,48 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
-
-public function secret_sharing_plugin_init() {
-    // Your initialization code goes here.
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
-add_action('init', 'secret_sharing_plugin_init');
+class RunthingsSecretsPlugin {
+
+    public function __construct() {
+        add_action('init', [$this, 'init']);
+    }
+
+    public function init() {
+    }
+
+    public function activate() {
+        global $wpdb;
+
+        $table_name = $wpdb->prefix . 'runthings_secrets';
+        $charset_collate = $wpdb->get_charset_collate();
+        
+        $sql = "CREATE TABLE $table_name (
+          id int(11) NOT NULL AUTO_INCREMENT,
+          uuid varchar(255) NOT NULL,
+          secret text NOT NULL,
+          max_views int(11) NOT NULL,
+          views int(11) NOT NULL,
+          expiration datetime NOT NULL,
+          created_at datetime NOT NULL,
+          PRIMARY KEY  (id)
+        ) $charset_collate;";
+      
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+
+    public function deactivate() {
+        // delete table(s)
+    }
+
+    public function uninstall() {
+    }
+}
+
+$runthings_secrets = new RunthingsSecretsPlugin();
+
+register_activation_hook( __FILE__, array( $runthings_secrets, 'activate' ) );
