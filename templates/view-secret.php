19a8179c051b017fa98ad7ef4ee1ad8130028cb6
Matthew Harris <rtpharry@hotmail.com>
2023-03-29 22:17:42 +0100

    feat(view-secret): i18n compatibility

--- templates/view-secret.php
+++ templates/view-secret.php
@@ -1,5 +1,5 @@
 <?php if($secret->is_error) : ?>
-    <p><strong>ERROR:</strong> <?php echo $secret->error_message; ?></p>
+    <p><strong><?php _e('ERROR:', 'runthings-secrets'); ?></strong> <?php echo $secret->error_message; ?></p>
 <?php else: ?>
-    <p>Your secret is: <?php echo $secret->secret; ?></p>
+    <p><?php echo sprintf(__('Your secret is: %s', 'runthings-secrets'), $secret->secret); ?></p>
 <?php endif; ?>
