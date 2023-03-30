9162d914f5d9242138775e25e14f589bdac7d845
Matthew Harris <rtpharry@hotmail.com>
2023-03-30 09:25:22 +0100

    feat(secret-created): use view page option for url

--- templates/secret-created.php
+++ templates/secret-created.php
@@ -1,8 +1,8 @@
 <?php
 
 // Generate the viewing URL.
-$viewing_url = site_url('view-secret/?secret=' . $uuid);
+$view_page_id = get_option('runthings_secrets_view_page');
+$viewing_url = get_permalink($view_page_id) . '?secret=' . $uuid;
 $viewing_link = '<a href="' . $viewing_url . '">' . $viewing_url . '</a>';
-
 ?>
 <p><?php echo sprintf(__('Your secret sharing URL: %s', 'runthings-secrets'), $viewing_link); ?></p>
