71beb42f5db5f2c205304285f12e29cadf603776
Matthew Harris <rtpharry@hotmail.com>
2023-04-02 19:46:29 +0100

    refactor(add-secret): wrap in div not p

--- templates/add-secret-form.php
+++ templates/add-secret-form.php
@@ -4,20 +4,20 @@ $default_max_views = 5;
 ?>
 <form method="post">
     <?php wp_nonce_field('runthings_secrets_add', 'runthings_secrets_add_nonce'); ?>
-    <p>
+    <div>
         <label for="secret"><?php _e('Secret:', 'runthings-secrets'); ?></label>
         <textarea name="secret" required></textarea>
-    </p>
-    <p>
+    </div>
+    <div>
         <label for="expiration"><?php _e('Expiration date:', 'runthings-secrets'); ?></label>
         <input type="date" name="expiration" required min="<?php echo date('Y-m-d'); ?>" value="<?php echo esc_attr($default_expiration); ?>">
-    </p>
-    <p>
+    </div>
+    <div>
         <label for="max_views"><?php _e('Maximum number of views:', 'runthings-secrets'); ?></label>
         <input type="number" name="max_views" min="1" max="10" required value="<?php echo esc_attr($default_max_views); ?>">
-    </p>
-    <p>
+    </div>
+    <div>
         <input type="hidden" name="recaptcha_token" id="recaptcha_token">
         <input type="submit" value="<?php _e('Submit', 'runthings-secrets'); ?>">
-    </p>
+    </div>
 </form>
