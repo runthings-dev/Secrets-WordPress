28b7ee8cdb96a0a760ae65e61661aa26dac620c7
Matthew Harris <rtpharry@hotmail.com>
2023-04-12 12:38:01 +0100

    feat(clipboard-copy): use modern approach when available

--- js/runthings-secrets.js
+++ js/runthings-secrets.js
@@ -1,10 +1,36 @@
-document.getElementById('copy-to-clipboard').addEventListener('click', function() {
-    const viewingUrlInput = document.getElementById('viewing-url');
-    viewingUrlInput.select();
-    viewingUrlInput.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand('copy');
-});
+document
+  .getElementById("copy-to-clipboard")
+  .addEventListener("click", function () {
+    const viewingUrlInput = document.getElementById("viewing-url");
 
-document.getElementById('viewing-url').addEventListener('click', function() {
-    this.select();
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      const textToCopy = viewingUrlInput.value;
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => {
+          console.log("Text copied to clipboard");
+        })
+        .catch((err) => {
+          console.error("Failed to copy text: ", err);
+        });
+    } else {
+      // Fallback for older browsers
+      viewingUrlInput.select();
+      viewingUrlInput.setSelectionRange(0, 99999); // For mobile devices
+
+      try {
+        const successful = document.execCommand("copy");
+        if (successful) {
+          console.log("Text copied to clipboard");
+        } else {
+          console.error("Failed to copy text");
+        }
+      } catch (err) {
+        console.error("Failed to copy text: ", err);
+      }
+    }
+  });
+
+document.getElementById("viewing-url").addEventListener("click", function () {
+  this.select();
 });
