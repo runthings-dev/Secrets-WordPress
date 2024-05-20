cf82e08e78ba414ca3b687206f85f5314666eebf
Matthew Harris <rtpharry@hotmail.com>
2024-05-20 23:28:39 +0100

    feat(build-zip): honor distignore via rsync

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Plugin slug, directory name of the plugin
+# Get the absolute path to the current directory
+PLUGIN_DIR=$(pwd)
 PLUGINSLUG="runthings-secrets"
 
 # Create the build directory if it doesn't exist
-mkdir -p build
+mkdir -p ${PLUGIN_DIR}/build
 
-# Create a zip file excluding the files and directories specified in .distignore
-zip -r build/${PLUGINSLUG}.zip . -x@.distignore
+# Create a temporary directory to stage the files to be zipped
+TEMP_DIR=$(mktemp -d)
+
+# Copy all files to the temporary directory, excluding the patterns in .distignore
+rsync -av --exclude-from=${PLUGIN_DIR}/.distignore ${PLUGIN_DIR}/ ${TEMP_DIR}/
+
+# Create the zip file from the temporary directory
+cd ${TEMP_DIR}
+zip -r ${PLUGIN_DIR}/build/${PLUGINSLUG}.zip .
+
+# Clean up the temporary directory
+rm -rf ${TEMP_DIR}
