8f2cc522c237d343d8ab7cf18a7e9c7f2b05d5ef
Matthew Harris <rtpharry@hotmail.com>
2024-05-20 23:42:49 +0100

    feat(build-zip): define constants for commonly used paths

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -2,6 +2,9 @@
 
 PLUGIN_DIR=$(pwd)
 PLUGINSLUG="runthings-secrets"
+BUILD_DIR="${PLUGIN_DIR}/build"
+DISTIGNORE="${PLUGIN_DIR}/.distignore"
+MAKEPOT_SCRIPT="${PLUGIN_DIR}/bin/makepot.sh"
 
 # Check if the script is being run from the bin directory
 if [[ $(basename "$PLUGIN_DIR") == "bin" ]]; then
@@ -13,14 +16,14 @@ fi
 
 # Run makepot.sh to generate/update translation files
 echo "Running makepot.sh to generate/update translation files..."
-if ! ./bin/makepot.sh; then
+if ! ${MAKEPOT_SCRIPT}; then
   echo "Error: makepot.sh failed."
   exit 1
 fi
 
 # Create the build directory if it doesn't exist
 echo "Creating build directory..."
-mkdir -p ${PLUGIN_DIR}/build
+mkdir -p ${BUILD_DIR}
 
 # Create a temporary directory to stage the files to be zipped
 TEMP_DIR=$(mktemp -d)
@@ -28,7 +31,7 @@ echo "Created temporary directory at ${TEMP_DIR}"
 
 # Copy all files to the temporary directory, excluding the patterns in .distignore
 echo "Copying files to temporary directory, excluding patterns in .distignore..."
-if ! rsync -av --exclude-from=${PLUGIN_DIR}/.distignore ${PLUGIN_DIR}/ ${TEMP_DIR}/; then
+if ! rsync -av --exclude-from=${DISTIGNORE} ${PLUGIN_DIR}/ ${TEMP_DIR}/; then
   echo "Error: rsync failed."
   rm -rf ${TEMP_DIR}
   exit 1
@@ -37,13 +40,13 @@ fi
 # Create the zip file from the temporary directory
 cd ${TEMP_DIR}
 echo "Creating zip file..."
-if ! zip -r ${PLUGIN_DIR}/build/${PLUGINSLUG}.zip .; then
+if ! zip -r ${BUILD_DIR}/${PLUGINSLUG}.zip .; then
   echo "Error: zip failed."
   cd ${PLUGIN_DIR}
   rm -rf ${TEMP_DIR}
   exit 1
 fi
-echo "Zip file created at ./build/${PLUGINSLUG}.zip"
+echo "Zip file created at ${BUILD_DIR}/${PLUGINSLUG}.zip"
 
 # Clean up the temporary directory
 cd ${PLUGIN_DIR}
