7ad19ff305b2bc4e0a912388a896942bedd3002b
Matthew Harris <rtpharry@hotmail.com>
2025-11-08 15:34:48 +0700

    docs(meta): 2025 copyright year range

--- runthings-secrets.php
+++ runthings-secrets.php
@@ -18,7 +18,7 @@ namespace RunthingsSecrets;
  * Domain Path: /languages
  */
 /*
-Copyright 2023-2024 Matthew Harris
+Copyright 2023-2025 Matthew Harris
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 3, as
